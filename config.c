8af4a68ca1c4ab5716a83c33ef00ec7ad590e56e
Jxstxs <juleb0202@gmail.com>
2023-05-06 22:35:09 +0200

    fix: updated scripts in config.h

--- config.c
+++ config.c
@@ -4,16 +4,11 @@
 #include "util.h"
 
 Block blocks[] = {
-    {"sb-mail",    600,  1 },
-    {"sb-music",   0,    2 },
-    {"sb-disk",    1800, 3 },
-    {"sb-memory",  10,   4 },
-    {"sb-loadavg", 5,    5 },
-    {"sb-mic",     0,    6 },
-    {"sb-record",  0,    7 },
-    {"sb-volume",  0,    8 },
-    {"sb-battery", 5,    9 },
-    {"sb-date",    1,    10},
+    {"mic.sh",       0,   1},
+    {"volume.sh",    0,   2},
+    {"battery.sh",   300, 3},
+    {"date.sh",      1,   4},
+    {"systablet.sh", 0,   5},
 };
 
 const unsigned short blockCount = LEN(blocks);
